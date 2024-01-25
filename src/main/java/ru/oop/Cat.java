65dcec36a10e6d2f6a66ea39d89f910ac1638953
tvv <tvv_yar@mail.ru>
2024-01-25 17:38:38 +0300

    7.6 Поля объекта

---
+++ src/main/java/ru/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("Я - " + this.name + ". Я ем " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Черныш");
+        black.eat("fish");
+        black.show();
+    }
+}
