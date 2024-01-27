7d63ac99a9cacd14a40f1ec4135f88372b193a1d
tvv <tvv_yar@mail.ru>
2024-01-27 21:20:37 +0300

    8.2 вызов конструктора у super

--- src/main/java/ru/inheritance/Programmer.java
+++ src/main/java/ru/inheritance/Programmer.java
@@ -4,7 +4,7 @@ public class Programmer extends Engineer {
     private String programLang;
 
     public Programmer(String programLang, int experience, boolean degree) {
-        super(experience,degree);
+        super(experience, degree);
         this.programLang = programLang;
     }
 }
