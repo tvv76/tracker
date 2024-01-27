926ec63558dc22892d662ac6dc5e11859b8de242
tvv <tvv_yar@mail.ru>
2024-01-27 19:54:28 +0300

    8.3 переопределение метода

---
+++ src/main/java/ru/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.inheritance;
+
+public class JSONReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return String.format("{" + ln
+                + "\t\"name\" : \"%s\"," + ln
+                + "\t\"body\" : \"%s\"" + ln
+                + "}", name, body);
+    }
+
+}
