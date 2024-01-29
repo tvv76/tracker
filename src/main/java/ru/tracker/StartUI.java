8e76e4c8d222136d5ca67ddacf8baa27475371d8
tvv <tvv_yar@mail.ru>
2024-01-29 19:35:12 +0300

    9.12 toString()

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -1,11 +1,10 @@
 package ru.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter));
+        item.setId(111);
+        item.setName("Test item");
+        System.out.println(item);
     }
 }
