7200587bf5fa7fe84aa41ccc9480fcd6cd997a48
tvv <tvv_yar@mail.ru>
2024-01-29 09:03:24 +0300

    9.5 удаление моделей из массива

--- src/main/java/ru/pojo/ShopDrop.java
+++ src/main/java/ru/pojo/ShopDrop.java
@@ -2,10 +2,10 @@ package ru.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        for (int i = index; i < products.length; i++) {
-            products[i] = i < products.length - 1 ? products[i + 1] : null;
-            if (products[i] == null) break;
+        for (int i = index; i < products.length-1; i++) {
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
