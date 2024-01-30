08016b3f0e5a83fca0d06792e86d61aadd6ae7f8
tvv <tvv_yar@mail.ru>
2024-01-30 17:07:59 +0300

    9.7 tracker хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -18,7 +18,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[100];
+        Item[] rsl = new Item[size];
         int count = 0;
 
         for (int i = 0; i < size; i++) {
