1bdcb279b22627561df78ea0c2fbf2524a290d43
tvv <tvv_yar@mail.ru>
2024-01-30 09:18:04 +0300

    9.12 toString()

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -2,9 +2,7 @@ package ru.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        item.setId(111);
-        item.setName("Test item");
+        Item item = new Item(111, "Test item");
         System.out.println(item);
     }
 }
