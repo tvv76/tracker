3e1e30ec763aa514572a5ae9468193f2573e335c
tvv <tvv_yar@mail.ru>
2024-02-02 18:57:29 +0300

    11.4 тестирование исключений

---
+++ src/test/java/ru/ex/FactorialTest.java
@@ -0,0 +1,17 @@
+package ru.ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactorialTest {
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Factorial().calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Аргумент должен быть положительным числом или 0.");
+    }
+}
