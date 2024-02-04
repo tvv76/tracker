4a86bc73f3fede567a0d6ebe17469607dd987c00
tvv <tvv_yar@mail.ru>
2024-02-04 09:37:58 +0300

    11.9 тесты на StartUI

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -187,4 +187,28 @@ class StartUITest {
                         + "=== Завершение программы ===" + ln
         );
     }
+
+    @Test
+    void whenInvalidExit() {
+        Input input = new MockInput(
+                new String[] {"3", "0"}
+        );
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new Exit(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "Неверный ввод, вы можете выбрать: 0 .. 0" + ln
+                        + "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+
+
+        );
+    }
 }
