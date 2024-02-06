33b9d41feaddd7c969d23c10769ea4a9e1371469
tvv <tvv_yar@mail.ru>
2024-02-06 10:36:18 +0300

    12.2 Singltone

---
+++ src/main/java/ru/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.tracker;
+
+public final class SingleTracker {
+
+    private static SingleTracker instance = null;
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstace() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
