179a42396703b6259dde1fc67131529eebe2a82c
tvv <tvv_yar@mail.ru>
2024-02-06 09:41:49 +0300

    11.12 if else throw

---
+++ src/main/java/ru/ex/Find.java
@@ -0,0 +1,11 @@
+package ru.ex;
+
+public class Find {
+    public static String get(String[] data, int index) {
+        if (index >= 0 && index < data.length) {
+            return data[index];
+        }
+
+        throw new IllegalArgumentException("Index out of bound");
+    }
+}
