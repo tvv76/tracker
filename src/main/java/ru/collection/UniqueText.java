cf74e5ede3e2c5661c8bea8c1240ce1f1c1eb8cd
tvv <tvv_yar@mail.ru>
2024-02-17 11:52:18 +0300

    14.3 Идентичные тексты

--- src/main/java/ru/collection/UniqueText.java
+++ src/main/java/ru/collection/UniqueText.java
@@ -1,21 +1,18 @@
 package ru.collection;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
-        boolean result = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
-        HashSet<String> check = new HashSet<>();
-        for (String word : origin) {
-            check.add(word);
-        }
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
         for (String word : text) {
             if (!check.contains(word)) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
