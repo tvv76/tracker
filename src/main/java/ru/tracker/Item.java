2482a6db85ff9f164618ea107fcd4084a3cab54c
tvv <tvv_yar@mail.ru>
2024-02-20 09:19:19 +0300

    17.2 Сортировка

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 
 public class Item {
     private int id;
@@ -50,4 +51,19 @@ public class Item {
                 ", created=" + created.format(FORMATTER) +
                 '}';
     }
+
+    public static class ItemAscByName implements Comparator<Item> {
+        @Override
+        public int compare(Item left, Item right) {
+            return left.getName().compareTo(right.getName());
+        }
+    }
+
+    public static class ItemDescByName implements Comparator<Item> {
+        @Override
+        public int compare(Item left, Item right) {
+            return right.getName().compareTo(left.getName());
+        }
+    }
+
 }
---
+++ src/test/java/ru/tracker/ItemTest.java
@@ -0,0 +1,66 @@
+package ru.tracker;
+
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemTest {
+    @Test
+    void whenAscSorListOfItems() {
+        Item item1 = new Item(1, "G");
+        Item item2 = new Item(2, "A");
+        Item item3 = new Item(3, "D");
+        Item item4 = new Item(4, "A");
+
+        List<Item> items = Arrays.asList(
+                item1,
+                item2,
+                item3,
+                item4
+        );
+
+        items.sort(new Item.ItemAscByName());
+
+        List<Item> expected = Arrays.asList(
+                item2,
+                item4,
+                item3,
+                item1
+        );
+        assertThat(items.get(0)).isEqualTo(expected.get(0));
+        assertThat(items.get(1)).isEqualTo(expected.get(1));
+        assertThat(items.get(2)).isEqualTo(expected.get(2));
+        assertThat(items.get(3)).isEqualTo(expected.get(3));
+    }
+
+    @Test
+    void whenDescSorListOfItems() {
+        Item item1 = new Item(1, "G");
+        Item item2 = new Item(2, "A");
+        Item item3 = new Item(3, "D");
+        Item item4 = new Item(4, "A");
+
+        List<Item> items = Arrays.asList(
+                item1,
+                item2,
+                item3,
+                item4
+        );
+
+        items.sort(new Item.ItemDescByName());
+
+        List<Item> expected = Arrays.asList(
+                item1,
+                item3,
+                item2,
+                item4
+        );
+        assertThat(items.get(0)).isEqualTo(expected.get(0));
+        assertThat(items.get(1)).isEqualTo(expected.get(1));
+        assertThat(items.get(2)).isEqualTo(expected.get(2));
+        assertThat(items.get(3)).isEqualTo(expected.get(3));
+    }
+}
