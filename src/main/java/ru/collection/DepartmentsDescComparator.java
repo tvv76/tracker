6d445d0bf804152d4e0f29cb9c974ecf14ca006e
tvv <tvv_yar@mail.ru>
2024-02-26 15:28:10 +0300

    19.1 отсортировать департаменты

--- src/main/java/ru/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/collection/DepartmentsDescComparator.java
@@ -5,18 +5,9 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] leftStructure = left.split("/", 2);
-        String[] rightStructure = right.split("/", 2);
+        String[] leftStructure = (left + "/ ").split("/", 2);
+        String[] rightStructure = (right + "/ ").split("/", 2);
         StringCompare compare = new StringCompare();
-
-        if (compare.compare(rightStructure[0], leftStructure[0]) != 0) {
-            return compare.compare(rightStructure[0], leftStructure[0]);
-        }
-
-        if (leftStructure.length != rightStructure.length) {
-            return leftStructure.length - rightStructure.length;
-        }
-
-        return compare.compare(leftStructure[1], rightStructure[1]);
+        return compare.compare(rightStructure[0], leftStructure[0]) != 0 ? compare.compare(rightStructure[0], leftStructure[0]) : compare.compare(leftStructure[1], rightStructure[1]);
     }
 }
