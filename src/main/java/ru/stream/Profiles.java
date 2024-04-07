1448a2b5254228926244e67feac122fc2e93675a
tvv <tvv_yar@mail.ru>
2024-04-07 09:54:07 +0300

    21.7 уникальность имен и сортировка

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -6,19 +6,15 @@ import java.util.List;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        List<Address> result = profiles.stream()
+        return profiles.stream()
                 .map(profile -> profile.getAddress())
                 .toList();
-        return result;
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        List<Address> result = collect(profiles).stream()
+        return collect(profiles).stream()
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .toList();
-
-        return result;
     }
-
 }
